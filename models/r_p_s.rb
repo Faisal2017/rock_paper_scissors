028bed965680a2456573f82bf26fb274ed577d19
Faisal <faisal_a@live.co.uk>
2017-06-13 08:54:11 +0100

    logic for defined but not used yet

--- models/r_p_s.rb
+++ models/r_p_s.rb
@@ -5,11 +5,15 @@ class Game
     @second_hand = second_hand
   end
 
-  def game_rules
-    return "Rock wins" if @first_hand = "rock" || @second_hand = "rock"
+  def game_rules()
+    return "Rock wins" if @first_hand == "rock" || @second_hand == "rock"
   end
 
+  # def game_rules()
+  #   return "Rock wins" if (@first_hand == "rock" || "scissors" ) && (@second_hand == "rock" || "scissors")
 
+  #   return "Scissor wins" if (@first_hand == "paper" || "scissors" ) && (@second_hand == "paper" || "scissors")
 
-
-end  
+  #   return "Paper wins" if (@first_hand == "rock" || "paper" ) && (@second_hand == "rock" || "paper")
+  # end
+  # end
