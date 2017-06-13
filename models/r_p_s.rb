eb8c0dcdc9a089d890a10307bf574e3ae2fcebaf
Faisal <faisal_a@live.co.uk>
2017-06-13 09:42:50 +0100

    changed logic function so now works

--- models/r_p_s.rb
+++ models/r_p_s.rb
@@ -5,15 +5,17 @@ class Game
     @second_hand = second_hand
   end
 
+  # def game_rules()
+  #   return "Rock wins" if @first_hand == "rock" || @second_hand == "rock"
+  # end
+  # end
+
   def game_rules()
-    return "Rock wins" if @first_hand == "rock" || @second_hand == "rock"
+    return "Rock wins" if (@first_hand == "rock" || @first_hand == "scissors" ) && (@second_hand == "rock" || @second_hand == "scissors")
+
+    return "Scissors wins" if (@first_hand == "paper" || @first_hand == "scissors" ) && (@second_hand == "paper" || @second_hand == "scissors")
+
+    return "Paper wins" if (@first_hand == "rock" || @first_hand == "paper" ) && (@second_hand == "rock" || @second_hand == "paper")
   end
 
-  # def game_rules()
-  #   return "Rock wins" if (@first_hand == "rock" || "scissors" ) && (@second_hand == "rock" || "scissors")
-
-  #   return "Scissor wins" if (@first_hand == "paper" || "scissors" ) && (@second_hand == "paper" || "scissors")
-
-  #   return "Paper wins" if (@first_hand == "rock" || "paper" ) && (@second_hand == "rock" || "paper")
-  # end
-  # end
+  end
